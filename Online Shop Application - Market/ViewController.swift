aa5d02051ecaa3ab65cfa97590ea8ca1e83f5aa4
erdem ozgur <erdmzgr@gmail.com>
2020-04-09 11:24:24 +0300

    Installing Pods
    Installing Pods

---
+++ Online Shop Application - Market/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Online Shop Application - Market
+//
+//  Created by Erdem Özgür on 9.04.2020.
+//  Copyright © 2020 Erdem Özgür. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Online Shop Application - Market/Pods-Online Shop Application - Market-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Online_Shop_Application___Market : NSObject
+@end
+@implementation PodsDummy_Pods_Online_Shop_Application___Market
+@end
